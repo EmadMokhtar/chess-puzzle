b25b8318192bb9f7b6ffeaf95a0d660ac84ce0f0
Emad Mokhtar <emad.m.habib@gmail.com>
2016-04-28 14:42:27 +0300

    Add Queen, King, Knight, Bishop, and Rook classes

--- core/models.py
+++ core/models.py
@@ -1,5 +1,4 @@
 
-
 class ChessBoard:
     """
     Chess Board
@@ -16,5 +15,91 @@ class ChessBoard:
 
         self.board = board
 
+    def __getitem__(self, position):
+        row, col = position
+        return self.board[row][col]
+
+    def __setitem__(self, position, value):
+        row, col = position
+        self.board[row][col] = value
+
     def __repr__(self):
         return '\n'.join(['|'.join([col for col in row]) for row in self.board])
+
+
+class AbstractChessPiece:
+    """
+    Abstract Chess Piece
+    """
+    symbol = 'X'
+    board = None
+
+    def __init__(self, board, row, col):
+        self.board = board
+        self.column = col
+        self.row = row
+        self.set_position(row, col)
+
+    @property
+    def position(self):
+        return 'X:{0}, Y:{1}'.format(self.row, self.column)
+
+    def set_position(self, row, col):
+        """
+        Set initial piece position
+        :param row: board row index
+        :param col: board column index
+        """
+        self.board[row, col] = self.symbol
+        self.row = row
+        self.column = col
+
+    def move_to(self, row, col):
+        """
+        Move the price to chess board cell
+        :param row: board row index
+        :param col: board column index
+        :return: new position
+        """
+        old_col = self.column
+        old_row = self.row
+        self.board[row, col] = self.symbol
+        self.row = row
+        self.column = col
+        self.board[old_row, old_col] = 'X'
+        return self.row, self.column
+
+
+class Knight(AbstractChessPiece):
+    """
+    Knight Chess Piece
+    """
+    symbol = 'N'
+
+
+class Queen(AbstractChessPiece):
+    """
+    Queen Chess Piece
+    """
+    symbol = 'Q'
+
+
+class Bishop(AbstractChessPiece):
+    """
+    Bishop Chess Piece
+    """
+    symbol = 'B'
+
+
+class Rook(AbstractChessPiece):
+    """
+    Rook Chess Piece
+    """
+    symbol = 'R'
+
+
+class King(AbstractChessPiece):
+    """
+    King Chess Piece
+    """
+    symbol = 'K'
--- tests/test_chessboard.py
+++ tests/test_chessboard.py
@@ -1,6 +1,6 @@
 import unittest
 
-from core.models import ChessBoard
+from core.models import ChessBoard, King, Queen, Rook, Bishop, Knight
 
 
 class ChessBoardTestCases(unittest.TestCase):
@@ -24,6 +24,56 @@ class ChessBoardTestCases(unittest.TestCase):
         new_board = ChessBoard(0, 0)
         expected_board = ''''''
         self.assertEqual(expected_board, repr(new_board))
+
+    def test_place_king_on_board_and_move_it(self):
+        new_board = ChessBoard(3, 3)
+        king = King(new_board, 0, 0)
+        self.assertEqual(King.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:0', king.position)
+        king.move_to(0, 2)
+        self.assertEqual(King.symbol, new_board[0, 2])
+        self.assertNotEqual(King.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:2', king.position)
+
+    def test_place_rook_on_board_and_move_it(self):
+        new_board = ChessBoard(3, 3)
+        rook = Rook(new_board, 0, 0)
+        self.assertEqual(Rook.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:0', rook.position)
+        rook.move_to(0, 2)
+        self.assertEqual(Rook.symbol, new_board[0, 2])
+        self.assertNotEqual(Rook.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:2', rook.position)
+
+    def test_place_queen_on_board_and_move_it(self):
+        new_board = ChessBoard(3, 3)
+        queen = Queen(new_board, 0, 0)
+        self.assertEqual(Queen.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:0', queen.position)
+        queen.move_to(0, 2)
+        self.assertEqual(Queen.symbol, new_board[0, 2])
+        self.assertNotEqual(Queen.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:2', queen.position)
+
+    def test_place_bishop_on_board_and_move_it(self):
+        new_board = ChessBoard(3, 3)
+        bishop = Bishop(new_board, 0, 0)
+        self.assertEqual(Bishop.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:0', bishop.position)
+        bishop.move_to(0, 2)
+        self.assertEqual(Bishop.symbol, new_board[0, 2])
+        self.assertNotEqual(Bishop.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:2', bishop.position)
+
+    def test_place_knight_on_board_and_move_it(self):
+        new_board = ChessBoard(3, 3)
+        knight = Knight(new_board, 0, 0)
+        self.assertEqual(Knight.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:0', knight.position)
+        knight.move_to(0, 2)
+        self.assertEqual(Knight.symbol, new_board[0, 2])
+        self.assertNotEqual(Knight.symbol, new_board[0, 0])
+        self.assertEqual('X:0, Y:2', knight.position)
 
 
 if __name__ == '__main__':
